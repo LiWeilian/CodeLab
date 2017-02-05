045df65fd73d3de00070da20af8cf54f7a8105d0
liwl <liweilian0822@163.com>
2017-02-05 22:12:13 +0800

    C#_AttributeDemo

---
+++ C#/20170205_AttributeDemo/AttributeDemo/HelpAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttributeDemo
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    class HelpAttribute: Attribute
+    {
+        public HelpAttribute(string desc)
+        {
+            this.Description = desc;
+        }
+
+        public string Description { get; private set; }
+    }
+}
