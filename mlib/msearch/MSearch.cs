324557c9479b4478fc5b871d565b2c6b60200aa9
liwl <liweilian0822@163.com>
2017-02-06 18:18:13 +0800

    mlib

--- mlib/msearch/MSearch.cs
+++ mlib/msearch/MSearch.cs
@@ -9,9 +9,33 @@ namespace msearch
 {
     public class MSearch
     {
-        public void SearchFiles(string libPath)
+        public List<FileInfo> SearchFiles(string libPath)
         {
-            
+            List<FileInfo> files = new List<FileInfo>();
+            if (!Directory.Exists(libPath))
+            {
+                return null;
+            }
+
+            DirectoryInfo dirInfo = new DirectoryInfo(libPath);
+            FileInfo[] fileInfos = dirInfo.GetFiles();
+            foreach (FileInfo f in fileInfos)
+            {
+                files.Add(f);
+            }
+
+            DirectoryInfo[] dirInfos = dirInfo.GetDirectories();
+            foreach (DirectoryInfo d in dirInfos)
+            {
+                List<FileInfo> fs = SearchFiles(d.FullName);
+
+                foreach (FileInfo f in fs)
+                {
+                    files.Add(f);
+                }
+            }
+
+            return files;
         }
     }
 }
---
+++ mlib/mtest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using msearch;
+
+namespace mtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MSearch ms = new MSearch();
+            List<FileInfo> files = ms.SearchFiles(@"E:\work_tempfiles\work_tempfiles_2017\20170206");
+            foreach (FileInfo f in files)
+            {
+                Console.WriteLine(f.FullName);
+            }
+            Console.ReadLine();
+        }
+    }
+}
