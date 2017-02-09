8cc753cadffa27e9d0013dd74807402d55447b20
liwl <liweilian0822@163.com>
2017-02-09 18:55:48 +0800

    shenruqianchu_wpf

---
+++ shenruqianchu_wpf/shenruqianchu_wpf/Chapter03/Human.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Chapter03
+{
+    [TypeConverterAttribute(typeof(StringToHumanTypeConverter))]
+    public class Human
+    {
+        public string Name { get; set; }
+        public Human Child { get; set; }
+    }
+}
---
+++ shenruqianchu_wpf/shenruqianchu_wpf/Chapter03/StringToHumanTypeConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter03
+{
+    public class StringToHumanTypeConverter: System.ComponentModel.TypeConverter
+    {
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            if (value is string)
+            {
+                Human h = new Human();
+                h.Name = value as string;
+                return h;
+            }
+            return base.ConvertFrom(context, culture, value);
+        }
+    }
+}
