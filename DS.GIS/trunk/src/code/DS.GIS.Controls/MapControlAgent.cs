70489ab4cbf22de825c7d7a55116def8ec25088c
liwl <liweilian0822@163.com>
2017-02-10 16:32:54 +0800

    ds.gis

---
+++ DS.GIS/trunk/src/code/DS.GIS.Controls/MapControlAgent.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using ESRI.ArcGIS.Controls;
+using ESRI.ArcGIS.esriSystem;
+
+namespace DS.GIS.Controls
+{
+    public class MapControlAgent: IMapControlAgent
+    {
+        private AxMapControl mapCtrl = null;
+        public MapControlAgent()
+        {
+            ESRI.ArcGIS.RuntimeManager.Bind(ESRI.ArcGIS.ProductCode.EngineOrDesktop);
+
+            IAoInitialize aoInit = new AoInitializeClass();
+            esriLicenseProductCode productCode = esriLicenseProductCode.esriLicenseProductCodeEngineGeoDB;
+            if (aoInit.IsProductCodeAvailable(productCode) == esriLicenseStatus.esriLicenseAvailable)
+            {
+                aoInit.Initialize(productCode);
+            }
+
+            mapCtrl = new AxMapControl();
+            mapCtrl.OnDoubleClick += MapCtrl_OnDoubleClick;
+        }
+
+        private void MapCtrl_OnDoubleClick(object sender, IMapControlEvents2_OnDoubleClickEvent e)
+        {
+            MessageBox.Show("This is an ArcGIS MapControl.");
+        }
+
+        public Control MapControl
+        {
+            get { return mapCtrl; }
+        }
+    }
+}
---
+++ DS.GIS/trunk/src/code/DS.GIS.MAIN/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DS.GIS.MAIN
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DS.GIS.Controls.MapControlAgent MapCtrlAgent = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            MapCtrlAgent = new Controls.MapControlAgent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            fhMainMap.Child = MapCtrlAgent.MapControl;
+        }
+    }
+}
