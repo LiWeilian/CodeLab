d2833647fb10de14f2199ef2682eb2d6cb60a1a6
liwl <liweilian0822@163.com>
2017-02-13 00:31:40 +0800

    ImproveCSharp

---
+++ ImproveCSharp/Chapter01BasicLang/Dynamic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter01BasicLang
+{
+    class Dynamic
+    {
+        public string Name { get; set; }
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
--- ImproveCSharp/Chapter01BasicLang/Program.cs
+++ ImproveCSharp/Chapter01BasicLang/Program.cs
@@ -10,12 +10,29 @@ namespace Chapter01BasicLang
     {
         static void Main(string[] args)
         {
+            CallDynamic();
+            Console.ReadLine();
+        }
+
+        static void CallCloneable()
+        {
             Employee mike = new Employee() { IDCode = "NB123", Age = 25, Department = new Department() { Name = "Dep1" } };
             Employee rose = mike.Clone() as Employee;
             Console.WriteLine(rose.Department.ToString());
             mike.Department.Name = "Dep2";
             Console.WriteLine(rose.Department.ToString());
-            Console.ReadLine();
+        }
+
+        static void CallDynamic()
+        {
+            /*
+            object dynamic = new Dynamic();
+            var Add = typeof(Dynamic).GetMethod("Add");
+            int i = (int)Add.Invoke(dynamic, new object[] { 1, 2});
+            */
+            dynamic dynamic = new Dynamic();
+            int i = dynamic.Add(1, 2);
+            Console.WriteLine(i.ToString());
         }
     }
 }
