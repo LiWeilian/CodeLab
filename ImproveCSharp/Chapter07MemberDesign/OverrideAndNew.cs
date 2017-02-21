9971ce18d29ffcd19c9ed9c6cafa787f3714ebac
liwl <liweilian0822@163.com>
2017-02-22 01:51:54 +0800

    ImproveCSharp

---
+++ ImproveCSharp/Chapter07MemberDesign/OverrideAndNew.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter07MemberDesign
+{
+    public class Shape
+    {
+        public virtual void MethodVirtual()
+        {
+            Console.WriteLine("Shape MethodVirtual");
+        }
+
+        public void Method()
+        {
+            Console.WriteLine("Shape Method");
+        }
+    }
+
+    public class Circle: Shape
+    {
+        public override void MethodVirtual()
+        {
+            Console.WriteLine("Circle override MethodVirtual");
+        }
+    }
+
+    public class Triangle: Shape
+    {
+        public new void MethodVirtual()
+        {
+            Console.WriteLine("Triangle new MethodVirtual");
+        }
+
+        public new void Method()
+        {
+            Console.WriteLine("Triangle new Method");
+        }
+    }
+}
---
+++ ImproveCSharp/Chapter07MemberDesign/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter07MemberDesign
+{
+    class Program
+    {
+        static void CallOverrideAndNew()
+        {
+            Shape s = new Circle();
+            s.MethodVirtual();
+            s.Method();
+
+            Circle c = new Circle();
+            (c as Shape).MethodVirtual();
+            c.Method();
+
+            Shape s2 = new Triangle();
+            s2.MethodVirtual();
+            s2.Method();
+            (s2 as Triangle).MethodVirtual();
+            (s2 as Triangle).Method();
+
+            Triangle t = new Triangle();
+            t.MethodVirtual();
+            t.Method();
+            (t as Shape).MethodVirtual();
+            (t as Shape).Method();
+
+        }
+        static void Main(string[] args)
+        {
+            CallOverrideAndNew();
+
+            Console.ReadLine();
+        }
+    }
+}
