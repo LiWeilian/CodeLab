100f603510f1589897ddf0eab823f0e68e39afae
liwl <liweilian0822@163.com>
2017-02-27 23:31:43 +0800

    ImproveCSharp

---
+++ ImproveCSharp/Chapter05Exception/InsteadOfErrorCode.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.ServiceModel;
+
+namespace Chapter05Exception
+{
+    class InsteadOfErrorCode
+    {
+        public void Saveuser(string user)
+        {
+            //SaveUserToFile(user);
+            //SaveUserToDB(user);
+
+            //throw new IOException("throw IOException");
+            //throw new UnauthorizedAccessException("throw UnauthorizedAccessException");
+            //throw new CommunicationException("throw CommunicationException");
+            throw new Exception("throw Exception");
+        }
+
+        public void Method()
+        {
+            string user = "aaa";
+
+            try
+            {
+                Saveuser(user);
+            }
+            catch (IOException e)
+            {
+                //
+                Console.Write(e.Message);
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                //
+                Console.Write(e.Message);
+            }
+            catch (CommunicationException e)
+            {
+                //
+                Console.Write(e.Message);
+            }
+            catch (Exception e)
+            {
+                //
+                Console.Write(e.Message);
+            }
+        }
+    }
+}
---
+++ ImproveCSharp/Chapter05Exception/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter05Exception
+{
+    class Program
+    {
+        static void CallInsteadOfErrorCode()
+        {
+            InsteadOfErrorCode iec = new InsteadOfErrorCode();
+            iec.Method();
+        }
+        static void Main(string[] args)
+        {
+            CallInsteadOfErrorCode();
+
+            Console.ReadLine();
+        }
+    }
+}
