093cc15f1d5a5af9d598c5857be91e0c98d157f3
liwl <liweilian0822@163.com>
2017-02-28 19:40:02 +0800

    msmef

---
+++ MSMEF/WpfLayout01/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfLayout01
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Button1.Click += Button1_Click;
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            Charts frmCharts = new Charts();
+            frmCharts.ShowDialog();
+        }
+    }
+}
