138de7f4e22364138bc78e35b345b3e92225e891
liwl <liweilian0822@163.com>
2017-03-14 19:55:29 +0800

    MapWindowDemo

--- MapWindowDemo/MainApp/Form1.Designer.cs
+++ MapWindowDemo/MainApp/Form1.Designer.cs
@@ -30,6 +30,11 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.axMap1 = new AxMapWinGIS.AxMap();
+            this.btnAddLayer = new System.Windows.Forms.Button();
+            this.btnZoomIn = new System.Windows.Forms.Button();
+            this.btnZoomOut = new System.Windows.Forms.Button();
+            this.btnPan = new System.Windows.Forms.Button();
+            this.btnFullExtent = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.axMap1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -42,11 +47,66 @@
             this.axMap1.Size = new System.Drawing.Size(549, 412);
             this.axMap1.TabIndex = 0;
             // 
+            // btnAddLayer
+            // 
+            this.btnAddLayer.Location = new System.Drawing.Point(600, 42);
+            this.btnAddLayer.Name = "btnAddLayer";
+            this.btnAddLayer.Size = new System.Drawing.Size(75, 23);
+            this.btnAddLayer.TabIndex = 1;
+            this.btnAddLayer.Text = "Add Layer";
+            this.btnAddLayer.UseVisualStyleBackColor = true;
+            this.btnAddLayer.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnZoomIn
+            // 
+            this.btnZoomIn.Location = new System.Drawing.Point(600, 71);
+            this.btnZoomIn.Name = "btnZoomIn";
+            this.btnZoomIn.Size = new System.Drawing.Size(75, 23);
+            this.btnZoomIn.TabIndex = 2;
+            this.btnZoomIn.Text = "Zoom In";
+            this.btnZoomIn.UseVisualStyleBackColor = true;
+            this.btnZoomIn.Click += new System.EventHandler(this.btnZoomIn_Click);
+            // 
+            // btnZoomOut
+            // 
+            this.btnZoomOut.Location = new System.Drawing.Point(600, 100);
+            this.btnZoomOut.Name = "btnZoomOut";
+            this.btnZoomOut.Size = new System.Drawing.Size(75, 23);
+            this.btnZoomOut.TabIndex = 3;
+            this.btnZoomOut.Text = "Zoom Out";
+            this.btnZoomOut.UseVisualStyleBackColor = true;
+            this.btnZoomOut.Click += new System.EventHandler(this.btnZoomOut_Click);
+            // 
+            // btnPan
+            // 
+            this.btnPan.Location = new System.Drawing.Point(600, 129);
+            this.btnPan.Name = "btnPan";
+            this.btnPan.Size = new System.Drawing.Size(75, 23);
+            this.btnPan.TabIndex = 4;
+            this.btnPan.Text = "Pan";
+            this.btnPan.UseVisualStyleBackColor = true;
+            this.btnPan.Click += new System.EventHandler(this.btnPan_Click);
+            // 
+            // btnFullExtent
+            // 
+            this.btnFullExtent.Location = new System.Drawing.Point(578, 158);
+            this.btnFullExtent.Name = "btnFullExtent";
+            this.btnFullExtent.Size = new System.Drawing.Size(115, 23);
+            this.btnFullExtent.TabIndex = 5;
+            this.btnFullExtent.Text = "Full Extent";
+            this.btnFullExtent.UseVisualStyleBackColor = true;
+            this.btnFullExtent.Click += new System.EventHandler(this.btnFullExtent_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(636, 479);
+            this.ClientSize = new System.Drawing.Size(705, 477);
+            this.Controls.Add(this.btnFullExtent);
+            this.Controls.Add(this.btnPan);
+            this.Controls.Add(this.btnZoomOut);
+            this.Controls.Add(this.btnZoomIn);
+            this.Controls.Add(this.btnAddLayer);
             this.Controls.Add(this.axMap1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -58,6 +118,11 @@
         #endregion
 
         private AxMapWinGIS.AxMap axMap1;
+        private System.Windows.Forms.Button btnAddLayer;
+        private System.Windows.Forms.Button btnZoomIn;
+        private System.Windows.Forms.Button btnZoomOut;
+        private System.Windows.Forms.Button btnPan;
+        private System.Windows.Forms.Button btnFullExtent;
     }
 }
 
--- MapWindowDemo/MainApp/Form1.cs
+++ MapWindowDemo/MainApp/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 using AxMapWinGIS;
+using MapWinGIS;
 
 namespace MainApp
 {
@@ -18,6 +19,36 @@ namespace MainApp
         {
             InitializeComponent();
             
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            axMap1.Clear();
+            Application.DoEvents();
+            MapWinGIS.Shapefile shpFile = new MapWinGIS.ShapefileClass();
+            shpFile.Open(@"E:\work_documents\工作文档-项目资料\广州自来水二期\实施\2015\20150612_yaocedian\yi\GEOGROUND.resrgn_fh_1.shp", null);
+            axMap1.AddLayer(shpFile, true);
+            Application.DoEvents();
+        }
+
+        private void btnZoomIn_Click(object sender, EventArgs e)
+        {
+            axMap1.CursorMode = tkCursorMode.cmZoomIn;
+        }
+
+        private void btnZoomOut_Click(object sender, EventArgs e)
+        {
+            axMap1.CursorMode = tkCursorMode.cmZoomOut;
+        }
+
+        private void btnPan_Click(object sender, EventArgs e)
+        {
+            axMap1.CursorMode = tkCursorMode.cmPan;
+        }
+
+        private void btnFullExtent_Click(object sender, EventArgs e)
+        {
+            axMap1.ZoomToMaxExtents();
         }
     }
 }
