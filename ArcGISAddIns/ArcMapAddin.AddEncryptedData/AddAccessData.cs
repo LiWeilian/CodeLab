175a38545272ad42d8a28c062d9517917bb94351
liwl <liweilian0822@163.com>
2017-03-29 02:15:44 +0800

    ArcGISAddIns

---
+++ ArcGISAddIns/ArcMapAddin.AddEncryptedData/AddAccessData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ArcMapAddin.AddEncryptedData
+{
+    public class AddAccessData : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public AddAccessData()
+        {
+        }
+
+        protected override void OnClick()
+        {
+            //
+            //  TODO: Sample code showing how to access button host
+            //
+            ArcMap.Application.CurrentTool = null;
+            MessageBox.Show("Add Access Data");
+        }
+        protected override void OnUpdate()
+        {
+            Enabled = ArcMap.Application != null;
+        }
+    }
+
+}
---
+++ ArcGISAddIns/ArcMapAddin.AddEncryptedData/AddEsriGDB.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+
+namespace ArcMapAddin.AddEncryptedData
+{
+    public class AddEsriGDB : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public AddEsriGDB()
+        {
+        }
+
+        protected override void OnClick()
+        {
+        }
+
+        protected override void OnUpdate()
+        {
+        }
+    }
+}
