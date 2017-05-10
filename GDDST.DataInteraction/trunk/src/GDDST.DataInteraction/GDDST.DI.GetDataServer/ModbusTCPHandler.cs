b799825d4ac15f87607d9a22d3b3738f5ac711b6
liwl <liweilian0822@163.com>
2017-05-10 18:34:48 +0800

    GDDST.DI

---
+++ GDDST.DataInteraction/trunk/src/GDDST.DataInteraction/GDDST.DI.GetDataServer/ModbusTCPHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GDDST.DI.GetDataServer
+{
+    class ModbusTCPHandler : CommHandler
+    {
+        public override byte[] GetRequestBytes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ProcessResponse(byte[] responeBytes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
