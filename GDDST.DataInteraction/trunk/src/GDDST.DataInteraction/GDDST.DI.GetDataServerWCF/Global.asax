462aee64df54fdd3c71487a508f1a3e945e4bdaf
liwl <liweilian0822@163.com>
2017-05-22 21:14:37 +0800

    GDDST.DI

---
+++ GDDST.DataInteraction/trunk/src/GDDST.DataInteraction/GDDST.DI.GetDataServerWCF/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GDDST.DI.GetDataServerWCF.Global" Language="C#" %>
--- GDDST.DataInteraction/trunk/src/GDDST.DataInteraction/GDDST.DI.NetConsoleDemo/ModbusRtuClient.cs
+++ GDDST.DataInteraction/trunk/src/GDDST.DataInteraction/GDDST.DI.NetConsoleDemo/ModbusRtuClient.cs
@@ -11,27 +11,50 @@ namespace GDDST.DI.NetConsoleDemo
     {
         static public void Run()
         {
-            SerialPort sp = new SerialPort();
-            Console.WriteLine("端口号：");
-            sp.PortName = Console.ReadLine();
-            sp.BaudRate = 9600;
-            sp.DataBits = 8;
+            #region 创建串口通信连接
+            SerialPort serialPort = new SerialPort();
+            Console.WriteLine("串行端口号：");
+            serialPort.PortName = Console.ReadLine();
+
+            Console.WriteLine("波特率：");
+            string sBaudRate = Console.ReadLine();
+            int iBaudRate;
+            if (int.TryParse(sBaudRate, out iBaudRate))
+            {
+                serialPort.BaudRate = iBaudRate;
+            }
+            else
+            {
+                serialPort.BaudRate = 9600;
+            }
+
+            Console.WriteLine("数据位(7|8)：");
+            string sDataBits = Console.ReadLine();
+            switch (sDataBits)
+            {
+                case "7":
+                    serialPort.DataBits = 7;
+                    break;
+                default:
+                    serialPort.DataBits = 8;
+                    break;
+            }
 
             Console.WriteLine("校验方式(NONE|ODD|EVEN)：");
             string parity = Console.ReadLine();
             switch (parity.ToUpper().Trim())
             {
                 case "NONE":
-                    sp.Parity = Parity.None;
+                    serialPort.Parity = Parity.None;
                     break;
                 case "ODD":
-                    sp.Parity = Parity.Odd;
+                    serialPort.Parity = Parity.Odd;
                     break;
                 case "EVEN":
-                    sp.Parity = Parity.Even;
+                    serialPort.Parity = Parity.Even;
                     break;
                 default:
-                    sp.Parity = Parity.None;
+                    serialPort.Parity = Parity.None;
                     break;
             }
 
@@ -40,64 +63,148 @@ namespace GDDST.DI.NetConsoleDemo
             switch (stopBits.ToUpper().Trim())
             {
                 case "0":
-                    sp.StopBits = StopBits.None;
+                    serialPort.StopBits = StopBits.None;
                     break;
                 case "1":
-                    sp.StopBits = StopBits.One;
+                    serialPort.StopBits = StopBits.One;
                     break;
                 case "2":
-                    sp.StopBits = StopBits.Two;
+                    serialPort.StopBits = StopBits.Two;
                     break;
                 default:
-                    sp.StopBits = StopBits.One;
+                    serialPort.StopBits = StopBits.One;
                     break;
             }
-            
-            sp.ReadTimeout = 500;
-            sp.WriteTimeout = -1;
+
+            serialPort.ReadTimeout = 500;
+            serialPort.WriteTimeout = -1;
             try
             {
-                if (!sp.IsOpen)
+                if (!serialPort.IsOpen)
                 {
-                    sp.Open();
+                    serialPort.Open();
                 }
+
+                Console.WriteLine(string.Format("已连接串行端口[{0}]\r\n", serialPort.PortName));
 
             }
             catch (Exception ex)
             {
-                sp = null;
+                serialPort = null;
                 Console.WriteLine(ex.Message);
                 return;
             }
-            
+            #endregion
+
             while (true)
             {
-                //Thread.Sleep(1000);
-                sp.DiscardInBuffer();
-                sp.DiscardOutBuffer();
+                #region 生成Modbus RTU请求数据，并发送至设备
+                string devAddr;
+                byte bDevAddr;
+                do
+                {
+                    Console.WriteLine("请输入设备地址码(1~128，默认1)：");
+                    devAddr = Console.ReadLine();
+                    if (devAddr.Trim() == string.Empty)
+                    {
+                        devAddr = "1";
+                    }
+                } while (!byte.TryParse(devAddr, out bDevAddr));
 
-                byte[] m = { 0x01, 0x03, 0x00, 0x01, 0x00, 0x02 };
-                uint crc16 = ModbusCRC16(m, m.Length);
 
-                byte[] crc16b = BitConverter.GetBytes(crc16);
+                string sFuncCode;
+                byte bFuncCode;
+                do
+                {
+                    Console.WriteLine("请输入功能码(1~128，默认3)：");
+                    sFuncCode = Console.ReadLine();
+                    if (sFuncCode.Trim() == string.Empty)
+                    {
+                        sFuncCode = "3";
+                    }
+                } while (!byte.TryParse(sFuncCode, out bFuncCode));
 
-                byte[] rtuData = new byte[8];
-                rtuData[0] = m[0];
-                rtuData[1] = m[1];
-                rtuData[2] = m[2];
-                rtuData[3] = m[3];
-                rtuData[4] = m[4];
-                rtuData[5] = m[5];
-                rtuData[6] = crc16b[0];
-                rtuData[7] = crc16b[1];
 
-                Console.WriteLine(string.Format("Write:\r\n{0}", BitConverter.ToString(rtuData)));
-                sp.Write(rtuData, 0, rtuData.Length);
+                string sStartAddr;
+                ushort iStartAddr;
+                do
+                {
+                    Console.WriteLine("请输入起始寄存器地址(0~65535)：");
+                    sStartAddr = Console.ReadLine();
+                } while (!ushort.TryParse(sStartAddr, out iStartAddr));
 
+                string sRegCount;
+                ushort iRegCount;
+                do
+                {
+                    Console.WriteLine("请输入读取寄存器数量(1~128)：");
+                    sRegCount = Console.ReadLine();
+                } while (!ushort.TryParse(sRegCount, out iRegCount) || (iRegCount > 128));
+
+                byte[] temp = new byte[6];
+                temp[0] = bDevAddr;
+                temp[1] = bFuncCode;
+
+                byte[] bStartAddr = BitConverter.GetBytes(iStartAddr);
+                temp[2] = bStartAddr[1];
+                temp[3] = bStartAddr[0];
+
+                byte[] bRegCount = BitConverter.GetBytes(iRegCount);
+                temp[4] = bRegCount[1];
+                temp[5] = bRegCount[0];
+
+                uint iCRC16 = ModbusCRC16(temp, temp.Length);
+                byte[] bCrc16 = BitConverter.GetBytes(iCRC16);
+
+                byte[] modbusRtuReq = new byte[8];
+                modbusRtuReq[0] = temp[0];
+                modbusRtuReq[1] = temp[1];
+                modbusRtuReq[2] = temp[2];
+                modbusRtuReq[3] = temp[3];
+                modbusRtuReq[4] = temp[4];
+                modbusRtuReq[5] = temp[5];
+                //CRC16 地位在前，高位在后
+                modbusRtuReq[6] = bCrc16[0];
+                modbusRtuReq[7] = bCrc16[1];
+                
+
+                try
+                {
+                    Console.WriteLine("正在发送 Modbus RTU 请求数据至Modbus设备...");
+                    Console.WriteLine(string.Format("数据内容：{0}\r\n", BitConverter.ToString(modbusRtuReq)));
+                    serialPort.Write(modbusRtuReq, 0, modbusRtuReq.Length);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("发送 Modbus RTU 请求数据至Modbus设备时发生错误：{0}\r\n", e.Message);
+                    continue;
+                }
+                #endregion
+
+                #region 读取设备返回数据
+                //延时，避免数据未返回就读取
                 Thread.Sleep(1000);
-                byte[] b = new byte[9];
-                sp.Read(b, 0, b.Length);
-                Console.WriteLine(string.Format("Read:\r\n{0}", BitConverter.ToString(b)));
+                byte[] respDataLen = new byte[2];
+                respDataLen[0] = modbusRtuReq[5];
+                respDataLen[1] = modbusRtuReq[4];
+                int iRespDataLen = BitConverter.ToUInt16(respDataLen, 0);
+                Console.WriteLine("回复数据长度：{0}", iRespDataLen);
+                byte[] modbusRtuResponse = new byte[5 + iRespDataLen * 2];
+                try
+                {
+                    Console.WriteLine("正在从Modbus设备接收 Modbus RTU 回复数据...");
+
+                    serialPort.Read(modbusRtuResponse, 0, modbusRtuResponse.Length);
+
+                    Console.WriteLine(string.Format("数据内容：{0}\r\n", BitConverter.ToString(modbusRtuResponse)));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("从Modbus设备接收 Modbus RTU 回复数据时发生错误：{0}\r\n", e.Message);
+                    
+                    continue;
+                }
+                #endregion
 
                 if (Console.KeyAvailable)
                 {
@@ -105,7 +212,7 @@ namespace GDDST.DI.NetConsoleDemo
                 }
             }
 
-            sp.Close();
+            serialPort.Close();
 
         }
 
