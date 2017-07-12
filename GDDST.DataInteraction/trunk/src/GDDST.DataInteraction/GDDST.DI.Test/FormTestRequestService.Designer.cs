c0b5a42135d0b1eafe16e361035956c2f7b5a9ac
liwl <liweilian0822@163.com>
2017-07-12 20:32:14 +0800

    GDDST.DI

---
+++ GDDST.DataInteraction/trunk/src/GDDST.DataInteraction/GDDST.DI.Test/FormTestRequestService.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace GDDST.DI.Test
+{
+    partial class FormTestRequestService
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtResponse = new System.Windows.Forms.TextBox();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRequestBody = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtInterval = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(561, 36);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "发送请求";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtResponse
+            // 
+            this.txtResponse.Location = new System.Drawing.Point(12, 331);
+            this.txtResponse.Multiline = true;
+            this.txtResponse.Name = "txtResponse";
+            this.txtResponse.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResponse.Size = new System.Drawing.Size(532, 212);
+            this.txtResponse.TabIndex = 1;
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(12, 36);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(532, 21);
+            this.txtURL.TabIndex = 2;
+            this.txtURL.Text = "http://172.16.1.2:8734/gddstdataserver/dataservice/modbustcp";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "URL:";
+            // 
+            // txtRequestBody
+            // 
+            this.txtRequestBody.Location = new System.Drawing.Point(12, 92);
+            this.txtRequestBody.Multiline = true;
+            this.txtRequestBody.Name = "txtRequestBody";
+            this.txtRequestBody.Size = new System.Drawing.Size(532, 164);
+            this.txtRequestBody.TabIndex = 4;
+            this.txtRequestBody.Text = "{\"ServerID\":\"GDDST001\",\"DeviceAddr\":\"1\",\"FunctionCode\":\"3\",\"StartAddr\":\"0\",\"RegCo" +
+    "unt\":\"10\"}";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Request Body:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 316);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Response Body:";
+            // 
+            // txtInterval
+            // 
+            this.txtInterval.Location = new System.Drawing.Point(561, 92);
+            this.txtInterval.Name = "txtInterval";
+            this.txtInterval.Size = new System.Drawing.Size(75, 21);
+            this.txtInterval.TabIndex = 7;
+            this.txtInterval.Text = "3000";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(563, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "时间间隔：";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(561, 132);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 9;
+            this.btnStop.Text = "停止";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // FormTestRequestService
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(788, 568);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtInterval);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtRequestBody);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.txtResponse);
+            this.Controls.Add(this.btnSend);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FormTestRequestService";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtResponse;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRequestBody;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtInterval;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ GDDST.DataInteraction/trunk/src/GDDST.DataInteraction/GDDST.DI.Test/FormTestRequestService.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Threading;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace GDDST.DI.Test
+{
+    public partial class FormTestRequestService : Form
+    {
+        private bool canRequest = true;
+        public FormTestRequestService()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int interval = 0;
+                if(!int.TryParse(txtInterval.Text, out interval))
+                {
+                    interval = 0;
+                }
+
+                if (interval > 0)
+                {
+                    btnSend.Enabled = false;
+                    btnStop.Enabled = true;
+                }
+
+                while (canRequest)
+                {
+                    //string postData = "{\"ServerID\":\"GDDST001\",\"DeviceAddr\":\"1\",\"FunctionCode\":\"3\",\"StartAddr\":\"0\",\"RegCount\":\"10\"}";
+                    string postData = txtRequestBody.Text;
+                    byte[] postDataByte = Encoding.UTF8.GetBytes(postData);
+
+                    //"http://172.16.1.2:8734/gddstdataserver/dataservice/modbustcp"
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(txtURL.Text);
+                    request.Method = "POST";
+                    request.ContentType = "application/json;charset=utf-8";
+                    request.ContentLength = postData.Length;
+
+                    using (Stream requestStream = request.GetRequestStream())
+                    {
+                        requestStream.Write(postDataByte, 0, postDataByte.Length);
+                    }
+
+                    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                    Stream responseStream = response.GetResponseStream();
+                    StreamReader sr = new StreamReader(responseStream);
+                    txtResponse.AppendText(DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss\r\n"));
+                    txtResponse.AppendText(sr.ReadToEnd());
+                    txtResponse.AppendText("\r\n\r\n");
+
+                    if (interval <= 0)
+                    {
+                        break;
+                    } else
+                    {
+                        Thread.Sleep(interval);
+                    }
+                    Application.DoEvents();
+                }
+            }
+            finally
+            {
+                btnSend.Enabled = true;
+                btnStop.Enabled = false;
+                canRequest = true;
+            }
+            
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            canRequest = false;
+        }
+    }
+}
